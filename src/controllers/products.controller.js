9a7f8142c48dccb27fd7a3da93309198744f9802
Esperanza <dulcecrugal@gmail.com>
2024-02-13 16:59:14 -0600

    Primer commit: Agrega archivos iniciales del proyecto

---
+++ src/controllers/products.controller.js
@@ -0,0 +1,43 @@
+import productDAO from "../DAO/productdao.js";
+export const getAll = (req, res) => {
+    productDAO.getAll()
+        .then(products => res.render('../src/views/index', { products }))
+        .catch(err => res.json({
+            status: "Server unavailable"
+        })
+        );
+}
+export const getOne = (req, res) => {
+    productDAO.getOne(req.params.barcode)
+        .then(result => {
+            !result ? res.json({
+                message: "product not found :/"
+            }) : res.render('../src/views/edit', { result });
+        })
+        .catch(err => res.json({
+            status: "Server unavailable"
+        })
+        );
+}
+
+export const insertOne = (req, res) => {
+    productDAO.insertOne(req.body)
+        .then(result => res.redirect('/api/products/'))
+        .catch(err => res.json({ status: "Server unavailable" }));
+}
+
+export const updateOne = (req, res) => {
+    productDAO.updateOne(req.params.barcode, req.body)
+        .then(result => res.redirect('/api/products/'))
+        .catch(err => res.json({ status: "Server unavailable" }));
+}
+
+export const deleteOne = (req, res) => {
+    productDAO.deleteOne(req.params.barcode)
+        .then(result => {
+            !result ? res.json({
+                message: "product not found"
+            }) : res.redirect('/api/products/');
+        })
+        .catch(err => res.json({ status: "Server unavailable" }));
+}
---
+++ src/routes/products.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import {getAll, getOne, insertOne,updateOne, deleteOne} from "../controllers/products.controller.js";
+
+const router=Router();
+
+router.get('/',getAll)
+router.get('/:barcode',getOne);
+router.post('/',insertOne);
+router.post('/:barcode',updateOne);
+router.get('/d/:barcode',deleteOne);
+export default router
+
